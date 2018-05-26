e387dfe5b8322afbf7084dbfb84a4bfe722de5d7
Ian <iang4llmeister@gmail.com>
2018-05-25 22:28:20 -0700

    continued writing one_turn in ultimate_ttt

--- tictactoe.py
+++ tictactoe.py
@@ -19,20 +19,19 @@ import sys
 
 class tictactoe():
   #Create play function to automate a single game
-  #Don't __init__ it so can use for ultimate tictac
   dividing_line = "-----------"
   play_line = " {} | {} | {} "
-  values = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-  current_player = 'X'
   winning_combos = [ [0,1,2], [3,4,5], [6,7,8], #Horizontal
                      [0,3,6], [1,4,7], [2,5,8], #Vertical
                      [0,4,8], [2,4,6] ]         #Diagonal
-  winner = None
 
-  #def __init__( self ):
-  #  self.dividing_line = "-----------"
-  #  self.play_line = " {} | {} | {} "
-  #  self.values = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+  #This stays a class attribute - I want all nine to use the same value 
+  current_player = 'X'
+
+  def __init__( self ):
+    #These need to be different for each instance
+    self.values = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+    self.winner = None
 
   def play( self ):
     while True:
@@ -42,12 +41,12 @@ class tictactoe():
         break
       self.swap_players()
       
-
   def one_turn( self ):
     self.draw()
-    self.select()
+    number = self.select()
     self.is_winning()
     self.swap_players()
+    return number 
 
   def draw( self ):
     row1 = self.play_line.format( self.values[6], self.values[7], self.values[8] )
@@ -61,7 +60,7 @@ class tictactoe():
       #If a combo has won, return True, congratulate winner
       if self.values[combo[0]] == self.values[combo[1]] == self.values[combo[2]]:
         self.winner = self.values[combo[0]]
-        print( '{win} WINS!  Congrats!'.format(win=self.winner) )
+        print( '{win} wins this small game!  Congrats!'.format(win=self.winner) )
         return True
     return False #Only gets here if no winners
 
@@ -76,6 +75,7 @@ class tictactoe():
       if self.validate( char ):
         self.values[ int(char) - 1 ] = self.current_player #Uhhh ... keep track of this
         break
+    return int(char)
 
   def swap_players( self ):
     if self.current_player == 'X':
@@ -88,7 +88,7 @@ class tictactoe():
     if not char.isdigit():
       print( 'Please enter an integer\n', file=sys.stderr )
       return False
-    #Elimintate digits < 1 and > 9
+    #Elimintate igits < 1 and > 9
     char = int(char)
     if char < 1 or char > 9:
       print( 'Please enter an integer from 1 to 9\n', file=sys.stderr )
@@ -99,37 +99,6 @@ class tictactoe():
       return False
   
     return True
-
-
-def select_spot( values, char ):
-  while True:
-    spot = input( 'Please choose the spot to fill in (or press h for help): ' )
-    if spot in ['h','H']:
-      draw_board( ['1','2','3','4','5','6','7','8','9'])
-      print( 'Please select the number that corresponds to the drawing above.\nIf the spot chosen has not been taken already, it will select it for {}.\nIf it has been chosen, it will complain and ask you to choose again.\n\n'.format(char) )
-    elif valid_number( spot, values, char ):
-      values[ int(spot) - 1 ] = char
-      break
-
-def valid_number( spot, values ):
-  #All remaining are digits
-  if not spot.isdigit():
-    print( 'Invalid character.  Please select a valid integer', file=sys.stderr )
-    return False
-  
-  spot = int( spot )
-  #All remaining are in the right area
-  if spot < 0 or spot > 10:
-    print( 'Invalid number.  Please select a number from 1 to 9', file=sys.stderr )
-    return False
-  
-  #All remaining aren't taken already
-  if values[spot - 1] in ['X','O']:
-    print( 'This spot has been taken', file=sys.stderr )
-    return False
-    
-  values[ spot - 1 ] = char
-
 
 if __name__ == '__main__':
   game = tictactoe()
--- ultimate_tictactoe.py
+++ ultimate_tictactoe.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from tictactoe import tictactoe
+import sys
 
 """
             |             |
@@ -41,11 +42,13 @@ from tictactoe import tictactoe
 """
 
 #Turn
-# Draw board
-# Select number
-# Check number for validity
-# Set number in values list
-# Check if has won - if so, break
+# Draw big board
+# If prev choice leads to completed board
+#  Find out which small_games are won, create small board with those selected in case it is needed
+#  Ask user to choose a board
+# Draw small board
+# Run turn on small board, saving selection
+# Check if has won small game - if so, break
 # Change user and repeat
 
 class ultimate_tictactoe():
@@ -56,17 +59,60 @@ class ultimate_tictactoe():
   smallseparator = '-----------'
   big_x = [ '   \   /', '    \ / ', '     X  ', '    / \ ', '   /   \\' ]
   big_o = [ '     _  ', '    / \ ', '   |   |', '   |   |', '    \_/ ' ]
-  
 
   #Bottom left first, to the right and up
-  games = [ tictactoe() for i in range(9) ]
+  current_player = 'X'
+  small_game = None
+  def __init__( self ):
+    self.games = [ tictactoe() for i in range(9) ]
+
+  def one_turn( self ):
+    #Draw big board
+    self.draw()
+    #First turn.  Select small game then select a spot on small game
+    if self.small_game == None:
+      if sys.version_info[0] == 2:
+        char = raw_input( 'No small game has been selected.  Please select one using the small board as a reference: ' )
+      else:
+        print( 'No small game has been selected.  Using the small board as a reference,' )
+        char = input( 'please select a game to start from: ' )
+    # If prev choice leads to completed board
+    if self.games[self.small_game].winner != None:
+      #Find which small_games are won, create small board to match 
+      tmp = tictactoe()
+      for _ in range(self.games):
+        if self.games.winner == 'X':
+          tmp.values[_] = 'X'
+        elif self.games.winner == 'O':
+          tmp.values[_] = 'O'
+      tmp.draw()
+      # Ask user to choose a board
+      print( "Your next move would be in a completed board.  Please select the next board to move from." )
+      self.small_game = tmp.select()
+    # Draw small board
+    self.games[self.small_game].draw()
+    # Run turn on small board, saving selection
+    self.small_game = self.games[self.small_game].one_turn()
+    # Check if has won small game
+    self.games[self.small_game].is_winning()
+    # Check if has won big game
+    self.is_winning()
+    self.swap_players()
+      
+  def is_winning( self ):
+    for combo in self.winning_combos:
+      #If a combo has won, return True, congratulate winner
+      if self.values[combo[0]] == self.values[combo[1]] == self.values[combo[2]]:
+        self.winner = self.values[combo[0]]
+        print( '{win} WINS!  Congrats!'.format(win=self.winner) )
+        return True
+    return False #Only gets here if no winners
 
   def draw( self ):
     pieces = []
     #Build lines to draw for each individual game
     #Left to right, down to up
     for game in self.games:
-      game.winner = 'O'
       if game.winner == 'X':
         pieces.append(self.big_x)
       elif game.winner == 'O':
@@ -101,7 +147,28 @@ class ultimate_tictactoe():
     all_lines = [self.first_last] + top_row + [self.intermediate] + middle_row + [self.intermediate] + bottom_row + [self.first_last]
     print( '\n'.join(all_lines) )
     #5 lines for big X or big O
+
+  def swap_players( self ):
+    if self.current_player == 'X':
+      self.current_player = 'O'
+    else:
+      self.current_player = 'X'
   
+  def validate( self, char ):
+    #Eliminate non-digits
+    if not char.isdigit():
+      print( 'Please enter an integer\n', file=sys.stderr )
+      return False
+    #Elimintate digits < 1 and > 9
+    char = int(char)
+    if char < 1 or char > 9:
+      print( 'Please enter an integer from 1 to 9\n', file=sys.stderr )
+      return False
+    #Eliminate already-chosen characters
+    if self.values[ char - 1 ] in ['X', 'O']:
+      print( 'That spot has already been taken.\n', file=sys.stderr )
+      return False
+    return True
 
 
 if __name__ == '__main__':
